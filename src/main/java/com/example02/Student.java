99306b0e1bbacb0c1a6e6fcbf7c7a13b3a080d7b
songsongyl <2444616264@qq.com>
2024-03-23 20:54:27 +0800

    experiment

--- src/main/java/com/example02/Student.java
+++ src/main/java/com/example02/Student.java
@@ -2,20 +2,20 @@ package com.example02;
 
 public class Student {
     private String name;
-    private String sex;
+    private Sex sex;
     public enum Sex{
         MALE, FEMALE
     }
     public  void setName(String name){
         this.name = name;
     }
-    public void setSex(String sex){
+    public void setSex(Sex sex){
         this.sex = sex;
     }
     public String getName(){
         return name;
     }
-    public String getSex(){
+    public Sex getSex(){
         return sex;
     }
 }
--- src/main/java/com/example02/Test.java
+++ src/main/java/com/example02/Test.java
@@ -5,9 +5,9 @@ public class Test {
         Student stu1 = new Student();
         Student stu2 = new Student();
         stu1.setName("syl");
-        stu1.setSex("FEMALE");
+        stu1.setSex(Student.Sex.FEMALE);
         stu2.setName("aaa");
-        stu2.setSex("MALE");
+        stu2.setSex(Student.Sex.MALE);
         System.out.println(stu1.getName() + " " + stu1.getSex());
         System.out.println(stu2.getName() + " " + stu2.getSex());
     }
