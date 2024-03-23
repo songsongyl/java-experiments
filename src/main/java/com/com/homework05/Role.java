940b1a11b96d16073aa221c31a02cd4464d714b7
songsongyl <2444616264@qq.com>
2024-03-23 21:17:29 +0800

    experiment

---
+++ src/main/java/com/com/homework05/Role.java
@@ -0,0 +1,18 @@
+package com.com.homework05;
+
+public class Role {
+    private String name;
+    private int id;
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+}
