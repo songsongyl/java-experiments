df7a0f72751ddcaf400ae2feaa2dc22f70b3de57
songsongyl <2444616264@qq.com>
2024-04-08 16:40:58 +0800

    experiment

---
+++ src/main/java/com/example10/Student.java
@@ -0,0 +1,40 @@
+package com.example10;
+
+public class Student {
+    public enum Sex{
+        MALE,FEMALE
+    }
+    private int id;
+    private String name;
+    private Sex sex;
+
+    public Student(int id, String name, Sex sex) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+}
---
+++ src/main/java/com/example10/Test.java
@@ -0,0 +1,7 @@
+package com.example10;
+
+public class Test {
+    public static void main(String[] args){
+
+    }
+}
