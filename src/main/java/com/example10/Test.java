58607df621da080d818c01125eb5351a63610582
songsongyl <2444616264@qq.com>
2024-04-15 22:33:38 +0800

    experiment

--- src/main/java/com/example10/Test.java
+++ src/main/java/com/example10/Test.java
@@ -14,14 +14,13 @@ import static com.example10.entity.Student.Sex.FEMALE;
 public class Test {
    public static final StudentService studentService = new StudentServiceImpl();
     public static void main(String[] args){
-    //addStudentTest();
+   // addStudentTest();
    //listStudentsByYearTest();
     //listStudentsNamesByYearBySexTest();
-     // listStudentsByYearSortedByIdTest();
-      //  listStudentsMapBySexTest();
-      //  listStudentsByYearMapByIdTest();
+      //listStudentsByYearSortedByIdTest();
+       // listStudentsMapBySexTest();
+       // listStudentsByYearMapByIdTest();
         removeStudentTest();
-      //  listStudentsByYearMapByIdTest();
     }
     private static void addStudentTest(){
         Student student1 = new Student(202222,FEMALE,"syl",2022);
@@ -63,7 +62,7 @@ public class Test {
 //        });
     }
     private static void listStudentsByYearMapByIdTest(){
-        Map<Integer, Student> map =  studentService.listStudentsByYearMapById(2022);
+        Map<Integer, Student> map =  studentService.listStudentsByYearMapById(2012);
         //map.forEach((integer, student) -> System.out.println(student.getId(),student.getName()));
         for(Map.Entry<Integer,Student> all:map.entrySet()){
             System.out.println(all.getKey() + " " + all.getValue().getName());
@@ -71,7 +70,7 @@ public class Test {
     }
     private static void removeStudentTest(){
         System.out.println(studentService.removeStudent(201007));
-//        DatabaseUtils.listStudents().removeIf(student -> 201007==student.getId());
+       //DatabaseUtils.listStudents().removeIf(student -> 202222==student.getId());
    List<Student> students = DatabaseUtils.listStudents();
         students.forEach(student -> System.out.println(student.getId()));
     }
